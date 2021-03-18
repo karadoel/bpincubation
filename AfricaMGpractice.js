4c29b697bb7844aa05ffdcd0c4793fae4755ccd2
Kara Doelling <76063189+karadoel@users.noreply.github.com>
2021-03-18 08:45:13 +0000

    Create AfricaMGpractice.js

---
+++ AfricaMGpractice.js
@@ -0,0 +1,24 @@
+// Questions
+var q = [
+	'What is the vision?',
+  'Why don&#39;t others do this already?',
+  'What keeps others from becoming fast followers?',
+  'What is the revenue model (pay-per-use, recurring)?',
+  'Are there potential synergies with RC&S sustainability strategy?',
+  'How does market size for O&G compare to other sectors, like offshore wind?',
+  'What are customer value levers? (price, scheduling/flexibility, carbon)',
+  'What have you learned from other companies or POCs?',
+  'Will net zero ambition be required from day 1?',
+  'How does your product work in more detail?',
+	'What is Saile&#39;s differentiator?',
+  'What challenges/obstacles do you face?',
+	'What are the switching costs for customers?',
+  'What new features will you build after your first launch?', 
+];
+
+// Tips
+var tips = [
+	'Try to have quick and concise answers.',
+	'Try to answer in 2 to 3 sentences.',
+	'Answer like you would in a conversation, this is not a presentation.',
+];
