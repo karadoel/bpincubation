9c3b2cff862594d4511b9a24ab2c1da688585f4f
Kara Doelling <76063189+karadoel@users.noreply.github.com>
2023-09-12 13:34:44 +0100

    Create sharktank.js
    new question set for DT Shark Tank investment event

---
+++ sharktank.js
@@ -0,0 +1,28 @@
+// Questions
+var q = [
+	'Why will your customers value this over other options?',
+	'What is the number 1 metric you will move?',
+	'Why should we invest?',
+	'What hurdles are you anticipating?',
+	'How are you understanding customer needs?',
+	'What is the size of the team?',
+	'How does your product/idea work?',
+	'What makes your idea different from existing options?',
+	'Who needs what you&#39;re building?',
+	'How do you know customers need what you&#39;re making?',
+	'What obstacles will you face and how will you overcome them?',
+	'What is your growth like?',
+	'What are the top things users want?',
+	'In what ways are you and your team resourceful?',
+
+];
+
+// Tips
+var tips = [
+	'Try to answer using new language each time you answer the question.',
+	'Try to have quick and concise answers.',
+	'Try to answer in 2 to 3 sentences.',
+	'Answer like you would in a conversation, this is not a presentation.',
+	'When talking to a potential investor, be substantive, and get straight to the point.',
+
+];
