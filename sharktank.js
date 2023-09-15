ac0d5253df3c901599a157e31ac5992baa075a24
Kara Doelling <76063189+karadoel@users.noreply.github.com>
2023-09-15 11:14:34 +0100

    Update sharktank.js

--- sharktank.js
+++ sharktank.js
@@ -14,17 +14,14 @@ var q = [
 	'How are you understanding Product needs?',
 	'Why will Product teams value this over other options?',
 	'Why isn&#39;t someone already doing this?',
-
-	'Does this compete with or replace existing Products?,
+	'Does this compete with or replace existing products?,
 	'What obstacles will you face and how will you overcome them?',
 	'What will you do if we don&#39;t fund you?',	
 	'What will you do if we only provide 50% funding?',	
 	'Six months from now, what&#39;s going to be your biggest problem?',	
 	'If you are funded, what is your next step?',
 	'How will you ensure you succeed?',
-
- 
-
+  
 ];
 
  
@@ -36,7 +33,5 @@ var tips = [
 	'Try to answer in 2 to 3 sentences.',
 	'Answer like you would in a conversation, this is not a presentation.',
 	'When talking, be substantive and get straight to the point.',
-
- 
 
 ];
